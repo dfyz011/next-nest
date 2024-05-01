875db7a0ab028e960c555dd3bdbb76cc726e1f26
Baynov Ivan <Jhon0@yandex.ru>
2024-05-01 22:21:10 +0800

    style: css for header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
+import styles from "./styles.module.css";
 
 export const Header = () => {
 	return (
-		<header>
-			<nav>
-				<ul>
-					<li>
-						<Link href="/">Organization Tree</Link>
+		<header className={styles.header}>
+			<nav className={styles.nav}>
+				<ul className={styles.nav__list}>
+					<li className={styles.nav__item}>
+						<Link href="/" className={styles.nav__link}>
+							Organization Tree
+						</Link>
 					</li>
 				</ul>
 			</nav>

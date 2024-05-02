f5099c298894a8cdc03c5f8ad7ef72caa2d38cc9
Baynov Ivan <Jhon0@yandex.ru>
2024-05-02 09:48:40 +0800

    Revert "Revert "feat: model context""
    This reverts commit f3c704d94be2103903cfe2b89830d8352a4bb296.

--- frontend/src/components/ButtonedModal/ButtonedModal.tsx
+++ frontend/src/components/ButtonedModal/ButtonedModal.tsx
@@ -1,7 +1,6 @@
-"use client";
-
 import { ReactNode, useState } from "react";
 import styles from "./styles.module.css";
+import { ModalContext } from "./ModalContext";
 
 type ModalProps = {
 	text: string;
@@ -20,18 +19,20 @@ export const ButtonedModal = ({ text, children }: ModalProps) => {
 	};
 
 	return (
-		<div>
-			<button onClick={openModal}>{text}</button>
-			{isOpen && (
-				<div className={styles["modal-overlay"]}>
-					<div className={styles.modal}>
-						<button onClick={closeModal} className={styles["close-button"]}>
-							x
-						</button>
-						{children}
+		<ModalContext.Provider value={{ closeModal }}>
+			<div>
+				<button onClick={openModal}>{text}</button>
+				{isOpen && (
+					<div className={styles["modal-overlay"]}>
+						<div className={styles.modal}>
+							<button onClick={closeModal} className={styles["close-button"]}>
+								x
+							</button>
+							{children}
+						</div>
 					</div>
-				</div>
-			)}
-		</div>
+				)}
+			</div>
+		</ModalContext.Provider>
 	);
 };
